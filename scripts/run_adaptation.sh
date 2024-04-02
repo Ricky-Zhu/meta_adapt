47feb11bfb62b6b25a7e2ca6ca6d71ebca9a18a9
Ricky-Zhu <zhuruiqi1@gmail.com>
2024-04-02 22:56:17 +0100

    lora adaptation

--- scripts/run_adaptation.sh
+++ scripts/run_adaptation.sh
@@ -1,6 +1,6 @@
 demo_num=10
 
-python ../adaptation/adapt_train.py adaptation.adapt_demo_num_each_task=$demo_num adaptation.adaptation_task_id=6
+python ../adaptation/adapt_train.py adaptation.adapt_demo_num_each_task=$demo_num adaptation.adaptation_task_id=5
 python ../adaptation/adapt_train.py adaptation.adapt_demo_num_each_task=$demo_num adaptation.adaptation_task_id=6
 python ../adaptation/adapt_train.py adaptation.adapt_demo_num_each_task=$demo_num adaptation.adaptation_task_id=7
 python ../adaptation/adapt_train.py adaptation.adapt_demo_num_each_task=$demo_num adaptation.adaptation_task_id=8
