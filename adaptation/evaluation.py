9701b6c3184fd3de6ed4b1eb46732477355e2215
Ricky-Zhu <zhuruiqi1@gmail.com>
2024-04-05 11:39:12 +0100

    lora adaptation

--- adaptation/evaluation.py
+++ adaptation/evaluation.py
@@ -17,21 +17,11 @@ import numpy as np
 import pprint
 from pre_training.pre_training_algo import PreTrainMultitask
 import time
-import torch
 import robomimic.utils.file_utils as FileUtils
-
 from libero.libero import get_libero_path
 from libero.libero.benchmark import get_benchmark
-from libero.libero.envs import OffScreenRenderEnv, SubprocVectorEnv, DummyVectorEnv
-from libero.libero.utils.time_utils import Timer
-from libero.libero.utils.video_utils import VideoWriter
 from libero.lifelong.algos import *
-from libero.lifelong.datasets import get_dataset, SequenceVLDataset, GroupedTaskDataset
-from libero.lifelong.metric import (
-    evaluate_loss,
-    evaluate_success,
-    raw_obs_to_tensor_obs,
-)
+
 from libero.lifelong.utils import (
     control_seed,
     safe_device,
@@ -41,91 +31,76 @@ from libero.lifelong.utils import (
 )
 from utils.task_creation import create_tasks
 from libero.lifelong.main import get_task_embs
-
+from utils.evaluation import *
 import robomimic.utils.obs_utils as ObsUtils
 import robomimic.utils.tensor_utils as TensorUtils
 from policy import *
-import time
 from glob import glob
-
-benchmark_map = {
-    "libero_10": "LIBERO_10",
-    "libero_spatial": "LIBERO_SPATIAL",
-    "libero_object": "LIBERO_OBJECT",
-    "libero_goal": "LIBERO_GOAL",
-}
-
-algo_map = {
-    "base": "Sequential",
-    "er": "ER",
-    "ewc": "EWC",
-    "packnet": "PackNet",
-    "multitask": "Multitask",
-}
-
-policy_map = {
-    "bc_rnn_policy": "BCRNNPolicy",
-    "bc_transformer_policy": "BCTransformerPolicy",
-    "bc_vilt_policy": "BCViLTPolicy",
-}
 
 
 def main():
-    # e.g., experiments/LIBERO_SPATIAL/Multitask/BCRNNPolicy_seed100/
+    def evaluate_one_repo_adaptor(pre_train_model_path, adaptor_model_path):
+        # load the pre-trained model and adaptor model
+        sd, pre_train_cfg, previous_mask = torch_load_model(
+            pre_train_model_path, map_location=None
+        )
+
+        # get the cfg
+        model_dict = torch.load(adaptor_model_path, map_location=None)
+        cfg = model_dict['cfg']
+        lora_model_sd = model_dict['state_dict']
+
+        which_bias_train = 'lora_only' if not cfg.adaptation.train_all_bias else 'all'
+
+        control_seed(cfg.seed)
+        cfg.folder = get_libero_path("datasets")
+        cfg.bddl_folder = get_libero_path("bddl_files")
+        cfg.init_states_folder = get_libero_path("init_states")
+
+        algo = safe_device(eval('PreTrainMultitask')(10, cfg), 'cuda')
+        algo.policy.previous_mask = previous_mask
+
+        algo.policy.load_state_dict(sd, strict=False)
+        algo.policy.load_state_dict(lora_model_sd, strict=False)
+
+        ##########################
+
+        if not hasattr(cfg.data, "task_order_index"):
+            cfg.data.task_order_index = 0
+
+        # get the benchmark the task belongs to
+        benchmark = get_benchmark(cfg.benchmark_name)(cfg.data.task_order_index)
+        descriptions = [benchmark.get_task(i).language for i in range(10)]
+        task_embs = get_task_embs(cfg, descriptions)
+        benchmark.set_task_embs(task_embs)
+
+        # load the dataset via using function get_dataset, so that the obsutils in robomimic can be initialized
+        all_obs_keys = []
+        for modality_name, modality_list in cfg.data.obs.modality.items():
+            all_obs_keys += modality_list
+
+        datasets_default_path = get_libero_path("datasets")
+        dataset_path = os.path.join(datasets_default_path, benchmark.get_task_demonstration(0))
+        ObsUtils.initialize_obs_utils_with_obs_specs({"obs": cfg.data.obs.modality})
+        shape_meta = FileUtils.get_shape_metadata_from_dataset(
+            dataset_path=dataset_path, all_obs_keys=all_obs_keys, verbose=False
+        )
+        ### ======================= start evaluation ============================
+        task_id = [cfg.adaptation.adaptation_task_id]
+
+        # cfg.eval.n_eval=20 if want to set a different eval num as that in the exp
+        success_rate = evaluate_success_all_init_condtions(cfg, algo, benchmark, task_id)
+        print(f'task id:{cfg.adaptation.adaptation_task_id}, demo num: {cfg.adaptation.adapt_demo_num_each_task}, success_rate: {success_rate}')
+
     pre_trained_model_path = '../scripts/experiments/LIBERO_OBJECT/PreTrainMultitask/BCTransformerPolicy_seed10000/run_003/multitask_model.pth'
-    adaptor_model_path = '../scripts/experiments/LIBERO_OBJECT/PreTrainMultitask/LoraBCTPolicy_seed10000/run_012/lora_model.pth'
+    adaptor_model_paths = '../scripts/experiments/LIBERO_OBJECT/PreTrainMultitask/LoraBCTPolicy_seed10000/'
 
-    # load the pre-trained model and adaptor model
-    sd, pre_train_cfg, previous_mask = torch_load_model(
-        pre_trained_model_path, map_location=None
-    )
-
-    # get the cfg
-    model_dict = torch.load(adaptor_model_path, map_location=None)
-    cfg = model_dict['cfg']
-    lora_model_sd = model_dict['state_dict']
-
-    which_bias_train = 'lora_only' if not cfg.adaptation.train_all_bias else 'all'
-
-    control_seed(cfg.seed)
-    cfg.folder = get_libero_path("datasets")
-    cfg.bddl_folder = get_libero_path("bddl_files")
-    cfg.init_states_folder = get_libero_path("init_states")
-
-    algo = safe_device(eval('PreTrainMultitask')(10, cfg), 'cuda')
-    algo.policy.previous_mask = previous_mask
-
-    algo.policy.load_state_dict(sd, strict=False)
-    algo.policy.load_state_dict(lora_model_sd, strict=False)
-
-    ##########################
-
-    if not hasattr(cfg.data, "task_order_index"):
-        cfg.data.task_order_index = 0
-
-    # get the benchmark the task belongs to
-    benchmark = get_benchmark(cfg.benchmark_name)(cfg.data.task_order_index)
-    descriptions = [benchmark.get_task(i).language for i in range(10)]
-    task_embs = get_task_embs(cfg, descriptions)
-    benchmark.set_task_embs(task_embs)
-
-    # load the dataset via using function get_dataset, so that the obsutils in robomimic can be initialized
-    all_obs_keys = []
-    for modality_name, modality_list in cfg.data.obs.modality.items():
-        all_obs_keys += modality_list
-
-    datasets_default_path = get_libero_path("datasets")
-    dataset_path = os.path.join(datasets_default_path, benchmark.get_task_demonstration(0))
-    ObsUtils.initialize_obs_utils_with_obs_specs({"obs": cfg.data.obs.modality})
-    shape_meta = FileUtils.get_shape_metadata_from_dataset(
-        dataset_path=dataset_path, all_obs_keys=all_obs_keys, verbose=False
-    )
-    ### ======================= start evaluation ============================
-    task_id = [cfg.adaptation.adaptation_task_id]
-
-    # cfg.eval.n_eval=20 if want to set a different eval num as that in the exp
-    success_rate = evaluate_success(cfg, algo, benchmark, task_id)
-    print(success_rate)
+    for root, dirs, files in os.walk(adaptor_model_paths):
+        for directory in dirs:
+            run_path = os.path.join(root, directory)
+            exp_paths = [folder for folder in glob(os.path.join(run_path, '*.pth'))]
+            for exp_path in exp_paths:
+                evaluate_one_repo_adaptor(pre_trained_model_path, exp_path)
 
 
 if __name__ == "__main__":
--- utils/evaluation.py
+++ utils/evaluation.py
@@ -16,13 +16,11 @@ def split_evaluation(cfg, algo, benchmark, task_ids):
 
 
 def split_evaluate_one_task_success(
-        cfg, algo, task, task_emb, task_id, seen_initial_list, unseen_initial_list, eval_num, sim_states=None, task_str=""
+        cfg, algo, task, task_emb, task_id, seen_initial_list, unseen_initial_list, eval_num, sim_states=None,
+        task_str=""
 ):
     """
-    Evaluate a single task's success rate
-    sim_states: if not None, will keep track of all simulated states during
-                evaluation, mainly for visualization and debugging purpose
-    task_str:   the key to access sim_states dictionary
+    evaluate the algo on seen initial and unseen conditions
     """
     with Timer() as t:
         if cfg.lifelong.algo == "PackNet":  # need preprocess weights for PackNet
@@ -125,12 +123,139 @@ def split_evaluate_one_task_success(
                 env.close()
                 gc.collect()
                 return num_success / len(init_list)
+
         seen_suc_rate = 0.
         unseen_suc_rate = 0.
         for i in range(eval_num):
             seen_suc_rate += evaluate_on_given_init_list(seen_initial_list)
             unseen_suc_rate += evaluate_on_given_init_list(unseen_initial_list)
-        seen_suc_rate = seen_suc_rate/eval_num
-        unseen_suc_rate = unseen_suc_rate/eval_num
+        seen_suc_rate = seen_suc_rate / eval_num
+        unseen_suc_rate = unseen_suc_rate / eval_num
     print(f"[info] seen: {seen_suc_rate} unseen: {unseen_suc_rate}")
     return seen_suc_rate, unseen_suc_rate
+
+
+def evaluate_success_all_init_condtions(cfg, algo, benchmark, task_ids, result_summary=None):
+    """
+    Evaluate the success rate for all task in task_ids for all 50 init conditions
+    """
+    algo.eval()
+    successes = []
+    for i in task_ids:
+        task_i = benchmark.get_task(i)
+        task_emb = benchmark.get_task_emb(i)
+        task_str = f"k{task_ids[-1]}_p{i}"
+        curr_summary = result_summary[task_str] if result_summary is not None else None
+        success_rate = evaluate_one_task_success_all_init_condition(
+            cfg, algo, task_i, task_emb, i, sim_states=curr_summary, task_str=task_str
+        )
+        successes.append(success_rate)
+    return np.array(successes)
+
+
+def evaluate_one_task_success_all_init_condition(
+        cfg, algo, task, task_emb, task_id, sim_states=None, task_str=""
+):
+    """
+    Evaluate a single task's success rate on all 50 init conditions
+    cuz the ram of GPU might not be available for running 50 envs in parallel, we separate them
+    """
+    with Timer() as t:
+        # turn on the mp for running envs in parallel
+        algo.eval()
+        cfg.eval.use_mp = True
+
+        # initiate evaluation envs
+        env_args = {
+            "bddl_file_name": os.path.join(
+                cfg.bddl_folder, task.problem_folder, task.bddl_file
+            ),
+            "camera_heights": cfg.data.img_h,
+            "camera_widths": cfg.data.img_w,
+        }
+
+        env_num = min(cfg.eval.num_procs, cfg.eval.n_eval) if cfg.eval.use_mp else 1
+        eval_loop_num = (cfg.eval.n_eval + env_num - 1) // env_num
+
+        # Try to handle the frame buffer issue
+        env_creation = False
+
+        count = 0
+        while not env_creation and count < 5:
+            try:
+                if env_num == 1:
+                    env = DummyVectorEnv(
+                        [lambda: OffScreenRenderEnv(**env_args) for _ in range(env_num)]
+                    )
+                else:
+                    env = SubprocVectorEnv(
+                        [lambda: OffScreenRenderEnv(**env_args) for _ in range(env_num)]
+                    )
+                env_creation = True
+            except:
+                time.sleep(5)
+                count += 1
+        if count >= 5:
+            raise Exception("Failed to create environment")
+
+        ### Evaluation loop
+        # get fixed init states to control the experiment randomness
+        init_states_path = os.path.join(
+            cfg.init_states_folder, task.problem_folder, task.init_states_file
+        )
+        init_states = torch.load(init_states_path)
+        num_success = 0
+
+        for i in range(eval_loop_num):
+            env.reset()
+            indices = np.arange(i * env_num, (i + 1) * env_num) % init_states.shape[0]
+            init_states_ = init_states[indices]
+
+            dones = [False] * env_num
+            steps = 0
+            algo.reset()
+            obs = env.set_init_state(init_states_)
+
+            # dummy actions [env_num, 7] all zeros for initial physics simulation
+            dummy = np.zeros((env_num, 7))
+            for _ in range(5):
+                obs, _, _, _ = env.step(dummy)
+
+            if task_str != "":
+                sim_state = env.get_sim_state()
+                for k in range(env_num):
+                    if i * env_num + k < cfg.eval.n_eval and sim_states is not None:
+                        sim_states[i * env_num + k].append(sim_state[k])
+
+            while steps < cfg.eval.max_steps:
+                steps += 1
+
+                data = raw_obs_to_tensor_obs(obs, task_emb, cfg)
+                actions = algo.policy.get_action(data)
+
+                obs, reward, done, info = env.step(actions)
+
+                # record the sim states for replay purpose
+                if task_str != "":
+                    sim_state = env.get_sim_state()
+                    for k in range(env_num):
+                        if i * env_num + k < cfg.eval.n_eval and sim_states is not None:
+                            sim_states[i * env_num + k].append(sim_state[k])
+
+                # check whether succeed
+                for k in range(env_num):
+                    dones[k] = dones[k] or done[k]
+
+                if all(dones):
+                    break
+
+            # a new form of success record
+            for k in range(env_num):
+                if i * env_num + k < cfg.eval.n_eval:
+                    num_success += int(dones[k])
+
+        success_rate = num_success / cfg.eval.n_eval
+        env.close()
+        gc.collect()
+    print(f"[info] evaluate task {task_id} takes {t.get_elapsed_time():.1f} seconds")
+    return success_rate
