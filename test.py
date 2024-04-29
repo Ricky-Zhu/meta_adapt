e032688002f354a0f3206a4e8eb6c37c557c5fd7
Ricky-Zhu <zhuruiqi1@gmail.com>
2024-04-29 14:09:19 +0100

    train online meta

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-a=True
-print(f"dsfs{a}"
-      f"dsd{a}")
+import numpy as np
+res = [[0.5 , 0.7 , 0.8 , 0.72 ,0.84],
+       [0.42 ,0.68 ,0.68 ,0.4  ,0.58],
+       [0.5,  0.58 ,0.7 , 0.56 ,0.52]]
+res = np.array(res)
+mean = np.mean(res,axis=0)
+std = np.std(res,axis=0)
+print(mean,std)
