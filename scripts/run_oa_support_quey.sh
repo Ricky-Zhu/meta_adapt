a32e07d33fde04d9b94361dbd7860fb57676ab88
Ricky-Zhu <zhuruiqi1@gmail.com>
2024-04-29 16:44:19 +0100

    train online meta

--- scripts/run_oa_support_quey.sh
+++ scripts/run_oa_support_quey.sh
@@ -1,10 +1,8 @@
 
 
-python ../online_adapt/online_continual_adapt.py -m adapt_demo_num_each_task=10 meta_update_epochs=50 meta_support_num=5 meta_query_num=25 random_meta=false seed=0,100,10000
+python ../online_adapt/online_continual_adapt.py -m adapt_demo_num_each_task=10 meta_update_epochs=100 meta_support_num=10 meta_query_num=15 random_meta=false seed=0,100,10000
 sleep 2
-python ../online_adapt/online_adapt_evaluation.py -m adapt_demo_num_each_task=10 meta_update_epochs=50 meta_support_num=5 meta_query_num=25 random_meta=false seed=0,100,10000
+python ../online_adapt/online_adapt_evaluation.py -m adapt_demo_num_each_task=10 meta_update_epochs=100 meta_support_num=10 meta_query_num=15 random_meta=false seed=0,100,10000
 
-python ../online_adapt/online_continual_adapt.py -m adapt_demo_num_each_task=10 meta_update_epochs=50 meta_support_num=5 meta_query_num=25 random_meta=true seed=0,100,10000
-sleep 2
-python ../online_adapt/online_adapt_evaluation.py -m adapt_demo_num_each_task=10 meta_update_epochs=50 meta_support_num=5 meta_query_num=25 random_meta=true seed=0,100,10000
+
 
