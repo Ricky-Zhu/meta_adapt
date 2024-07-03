225b53499e3479c8616f5facb4d6d9acf1966597
Ricky-Zhu <zhuruiqi1@gmail.com>
2024-07-03 23:18:52 +0100

    old vit architecture

--- scripts/run_pretrain_libero_90.sh
+++ scripts/run_pretrain_libero_90.sh
@@ -1,2 +1,3 @@
-python ../pre_training/trainer.py task_creation.task_suite="libero_90" benchmark_name="LIBERO_90" train.batch_size=16
-python ../pre_training/evaluate.py --task_suite object
+python ../pre_training/trainer.py task_creation.task_suite="libero_90" benchmark_name="LIBERO_90" train.batch_size=32
+sleep 2
+python ../pre_training/evaluate.py task_creation.task_suite="libero_90" benchmark_name="LIBERO_90" train.batch_size=32
