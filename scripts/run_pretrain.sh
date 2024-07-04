924f2416c350d45717ba0bdeabe5b9e14fc475ae
Ricky-Zhu <zhuruiqi1@gmail.com>
2024-07-04 17:01:49 +0100

    old vit architecture

--- scripts/run_pretrain.sh
+++ scripts/run_pretrain.sh
@@ -1,3 +1,3 @@
-python ../pre_training/trainer.py train.batch_size=32
+python ../pre_training/trainer.py train.batch_size=64
 sleep 2
-python ../pre_training/evaluate.py train.batch_size=32
+python ../pre_training/evaluate.py train.batch_size=64
